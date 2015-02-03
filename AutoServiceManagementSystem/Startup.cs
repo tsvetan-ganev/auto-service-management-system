739c9ee09c851e930774438dbe7253e0cd83e207
tsvetan-ganev <cecoganev94@hotmail.com>
2015-02-03 17:07:23 +0200

    Init

---
+++ AutoServiceManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoServiceManagementSystem.Startup))]
+namespace AutoServiceManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
