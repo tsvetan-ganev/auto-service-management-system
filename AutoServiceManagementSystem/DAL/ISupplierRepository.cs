c4d9e0d4aa2b2ab93a0e8736d032bba44c5aeef5
tsvetan-ganev <cecoganev94@hotmail.com>
2015-02-05 23:10:44 +0200

    Implementing repository pattern for the Supplier model.

---
+++ AutoServiceManagementSystem/DAL/ISupplierRepository.cs
@@ -0,0 +1,19 @@
+﻿using AutoServiceManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServiceManagementSystem.DAL
+{
+	public interface ISupplierRepository : IDisposable
+	{
+		IEnumerable<Supplier> GetSuppliers();
+		Supplier GetSupplierById(int? supplierId);
+		void InsertSupplier(Supplier supplier);
+		void DeleteSupplier(int supplierId);
+		void UpdateSupplier(Supplier supplier);
+		void Save();
+	}
+}
---
+++ AutoServiceManagementSystem/DAL/SupplierRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoServiceManagementSystem.Models;
+using System.Data.Entity;
+
+namespace AutoServiceManagementSystem.DAL
+{
+	public class SupplierRepository : ISupplierRepository, IDisposable
+	{
+		private ASMSContext context;
+
+		public SupplierRepository(ASMSContext context)
+		{
+			this.context = context;
+		}
+
+		#region ISupplierRepository Implementation
+
+		public IEnumerable<Supplier> GetSuppliers()
+		{
+			return context.Suppliers.ToList();
+		}
+
+		public Supplier GetSupplierById(int? supplierId)
+		{
+			return context.Suppliers.Find(supplierId);
+		}
+
+		public void InsertSupplier(Supplier supplier)
+		{
+			context.Suppliers.Add(supplier);
+		}
+
+		public void DeleteSupplier(int supplierId)
+		{
+			Supplier supplier = context.Suppliers.Find(supplierId);
+			context.Suppliers.Remove(supplier);
+		}
+
+		public void UpdateSupplier(Supplier supplier)
+		{
+			context.Entry(supplier).State = EntityState.Modified;
+		}
+
+		public void Save()
+		{
+			context.SaveChanges();
+		}
+
+		#endregion
+
+		private bool disposed = false;
+		
+		public virtual void Dispose(bool disposing)
+		{
+			if (!disposed && disposing)
+			{
+				context.Dispose();
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
