9b9a063432cf8bdc0ea646a91d2e61fba75451d6
tsvetan-ganev <cecoganev94@hotmail.com>
2015-02-05 17:12:37 +0200

    Code formating.

--- AutoServiceManagementSystem/Controllers/CarsController.cs
+++ AutoServiceManagementSystem/Controllers/CarsController.cs
@@ -11,130 +11,130 @@ using AutoServiceManagementSystem.DAL;
 
 namespace AutoServiceManagementSystem.Controllers
 {
-    public class CarsController : Controller
-    {
-	private ICarRepository carRepo;
-
-	public CarsController()
+	public class CarsController : Controller
 	{
-		this.carRepo = new CarRepository(new ASMSContext());
+		private ICarRepository carRepo;
+
+		public CarsController()
+		{
+			this.carRepo = new CarRepository(new ASMSContext());
+		}
+
+		// GET: Cars
+		public ActionResult Index()
+		{
+			return View(carRepo.GetCars());
+		}
+
+		// GET: Cars/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			Car car = carRepo.GetCarById(id);
+
+			if (car == null)
+			{
+				return HttpNotFound();
+			}
+			return View(car);
+		}
+
+		// GET: Cars/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Cars/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "CarId,Manufacturer,Model,PlateCode,VIN,Year,Mileage,Displacement,FuelType")] Car car)
+		{
+			if (ModelState.IsValid)
+			{
+				carRepo.InsertCar(car);
+				carRepo.Save();
+				return RedirectToAction("Index");
+			}
+
+			return View(car);
+		}
+
+		// GET: Cars/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			Car car = carRepo.GetCarById(id);
+
+			if (car == null)
+			{
+				return HttpNotFound();
+			}
+			return View(car);
+		}
+
+		// POST: Cars/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "CarId,Manufacturer,Model,PlateCode,VIN,Year,Mileage,Displacement,FuelType")] Car car)
+		{
+			if (ModelState.IsValid)
+			{
+				carRepo.UpdateCar(car);
+				carRepo.Save();
+				return RedirectToAction("Index");
+			}
+			return View(car);
+		}
+
+		// GET: Cars/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			Car car = carRepo.GetCarById(id);
+
+			if (car == null)
+			{
+				return HttpNotFound();
+			}
+			return View(car);
+		}
+
+		// POST: Cars/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Car car = carRepo.GetCarById(id);
+			carRepo.DeleteCar(id);
+			carRepo.Save();
+
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				carRepo.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 	}
-
-        // GET: Cars
-        public ActionResult Index()
-        {
-            return View(carRepo.GetCars());
-        }
-
-        // GET: Cars/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            Car car = carRepo.GetCarById(id);
-
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            return View(car);
-        }
-
-        // GET: Cars/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cars/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CarId,Manufacturer,Model,PlateCode,VIN,Year,Mileage,Displacement,FuelType")] Car car)
-        {
-            if (ModelState.IsValid)
-            {
-		carRepo.InsertCar(car);
-		carRepo.Save();
-                return RedirectToAction("Index");
-            }
-
-            return View(car);
-        }
-
-        // GET: Cars/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-	    Car car = carRepo.GetCarById(id);
-
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            return View(car);
-        }
-
-        // POST: Cars/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CarId,Manufacturer,Model,PlateCode,VIN,Year,Mileage,Displacement,FuelType")] Car car)
-        {
-            if (ModelState.IsValid)
-            {
-		carRepo.UpdateCar(car);
-		carRepo.Save();
-                return RedirectToAction("Index");
-            }
-            return View(car);
-        }
-
-        // GET: Cars/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            
-	    Car car = carRepo.GetCarById(id);
-	    
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            return View(car);
-        }
-
-        // POST: Cars/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-	    Car car = carRepo.GetCarById(id);
-	    carRepo.DeleteCar(id);
-	    carRepo.Save();
-
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-		carRepo.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
 }
