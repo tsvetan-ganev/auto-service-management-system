43daddd6ff0adee89277b18a42a106d47dfeef9f
Tsvetan Ganev <cecoganev94@hotmail.com>
2015-02-07 18:16:47 +0200

    Added seed data for the default initializer.

--- AutoServiceManagementSystem/Global.asax.cs
+++ AutoServiceManagementSystem/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoServiceManagementSystem.DAL;
+using AutoServiceManagementSystem.DAL.Initializers;
 
 namespace AutoServiceManagementSystem
 {
@@ -12,6 +15,8 @@ namespace AutoServiceManagementSystem
     {
         protected void Application_Start()
         {
+			Database.SetInitializer(
+				new DropCreateDatabaseIfModelChanges<ASMSContext>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- AutoServiceManagementSystem/Migrations/Configuration.cs
+++ AutoServiceManagementSystem/Migrations/Configuration.cs
@@ -1,112 +1,227 @@
 namespace AutoServiceManagementSystem.Migrations
 {
 	using AutoServiceManagementSystem.Models;
+	using AutoServiceManagementSystem.DAL;
+	using AutoServiceManagementSystem.Helpers;
 	using System;
 	using System.Data.Entity;
 	using System.Data.Entity.Migrations;
 	using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ASMSContext>
-    {
+	internal sealed class Configuration : DbMigrationsConfiguration<ASMSContext>
+	{
 		private readonly bool pendingMigrations;
 
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = true;
-			AutomaticMigrationDataLossAllowed = true;
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = true;
+			AutomaticMigrationDataLossAllowed = false;
 			var migrator = new DbMigrator(this);
 			pendingMigrations = migrator.GetPendingMigrations().Any();
-        }
+		}
 
-        protected override void Seed(AutoServiceManagementSystem.Models.ASMSContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+		protected override void Seed(ASMSContext context)
+		{
+			//  This method will be called after migrating to the latest version.
 
-			var bmwX6 = new Car
-			{
-				Manufacturer = "BMW",
-				Model = "X6",
-				FuelType = Car.Fuel.Diesel,
-				PlateCode = "P 6666 KH",
-				VIN = "5GZCZ43D13S812715",
-				Year = 2010
-			};
+			// adding cars
+			var manufacturers = ManufacturersList.Manufacturers;
+			var rand = new Random(DateTime.Now.Millisecond);
+			int min = 0;
+			int max = manufacturers.Count;
 
-			var lada = new Car
-			{
-				Manufacturer = "Lada",
-				Model = "Jigula",
-				FuelType = Car.Fuel.Petrol,
-				PlateCode = "P 7584 KH",
-				VIN = "LDZCZ43D13S812715",
-				Year = 1981
-			};
+			#region Cars Seed Data
+			context.Cars.AddOrUpdate(c => c.CarId,
+				new Car
+				{
+					CarId = 1,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "XXYYZZ",
+					VIN = "1D8HS58N93F387970",
+					FuelType = Car.Fuel.Diesel
+				},
+				new Car
+				{
+					CarId = 2,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "ZZZYYYXXX",
+					VIN = "JTHBC1KSXB5307152",
+					FuelType = Car.Fuel.Petrol
+				},
+				new Car
+				{
+					CarId = 3,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "ZKL",
+					VIN = "2D8GP74L03R827449",
+					FuelType = Car.Fuel.Petrol
+				},
+				new Car
+				{
+					CarId = 4,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "AEZM",
+					VIN = "SALTW12422A939559",
+					FuelType = Car.Fuel.Gas
+				},
+				new Car
+				{
+					CarId = 5,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "KKL",
+					VIN = "1GT121E84BF673442",
+					FuelType = Car.Fuel.Gas
+				},
+				new Car
+				{
+					CarId = 6,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "QWER",
+					VIN = "1FTRF14V48K950702",
+					FuelType = Car.Fuel.Diesel
+				},
+				new Car
+				{
+					CarId = 7,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "JGKLT",
+					VIN = "WBAEU33492P793624",
+					FuelType = Car.Fuel.Diesel
+				},
+				new Car
+				{
+					CarId = 8,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "NBGJK",
+					VIN = "1J8FF48W17D850420",
+					FuelType = Car.Fuel.Petrol
+				},
+				new Car
+				{
+					CarId = 9,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "BGFR",
+					VIN = "1FTPW12597F436264",
+					FuelType = Car.Fuel.Petrol
+				},
+				new Car
+				{
+					CarId = 10,
+					Manufacturer = manufacturers.ElementAt(rand.Next(min, max)),
+					EngineCode = "MKLBG",
+					VIN = "1GTHC39R9XF262667",
+					FuelType = Car.Fuel.Petrol
+				}
+			);
+			#endregion
 
-			var mercedes = new Car
-			{
-				Manufacturer = "Mercedes",
-				Model = "S-Klass",
-				FuelType = Car.Fuel.Diesel,
-				PlateCode = "P 777777 P",
-				VIN = "1GTGG25R721773607",
-				Year = 2014
-			};
+			#region Customers Seed Data
+			context.Customers.AddOrUpdate(c => c.CustomerId,
+				new Customer
+				{
+					CustomerId = 1,
+					FirstName = "Ivan",
+					LastName = "Georgiev",
+					PhoneNumber = "0816136258"
+				},
+				new Customer
+				{
+					CustomerId = 2,
+					FirstName = "Georgi Ivanov",
+					LastName = "Georgiev",
+					PhoneNumber = "0875259779"
+				},
+				new Customer
+				{
+					CustomerId = 3,
+					FirstName = "Petar",
+					LastName = "Petrov",
+					PhoneNumber = "0886472046"
+				},
+				new Customer
+				{
+					CustomerId = 4,
+					FirstName = "Maria",
+					LastName = "Ilieva",
+					PhoneNumber = "0836140833"
+				},
+				new Customer
+				{
+					CustomerId = 5,
+					FirstName = "Haralampi",
+					LastName = "Karaivanov",
+					PhoneNumber = "0853748786"
+				},
+				new Customer
+				{
+					CustomerId = 6,
+					FirstName = "Hristian",
+					LastName = "Georgiev",
+					PhoneNumber = "0863933157"
+				}, new Customer
+				{
+					CustomerId = 7,
+					FirstName = "Ivan",
+					LastName = "Ivanov",
+					PhoneNumber = "0819116973"
+				}, new Customer
+				{
+					CustomerId = 8,
+					FirstName = "Евлоги",
+					LastName = "Христов",
+					PhoneNumber = "0847900455"
+				}
+			);
+			#endregion
 
-			context.Cars.AddOrUpdate(lada);
-			context.Cars.AddOrUpdate(bmwX6);
-			context.Cars.AddOrUpdate(mercedes);
-
-			/// <summary>
-			/// Seeding a list of sample customers.
-			/// </summary>
-			
-
-			/// <summary>  
-			/// Seeding a list of sample spare parts suppliers
-			/// </summary>
-			context.Suppliers.Add(new Supplier
-			{
-				Name = "Euro07",
-				City = "Ruse",
-				DiscountPercentage = 40.0m,
-				WebsiteUrl = "http://euro07.bg"
-			});
-
-			context.Suppliers.Add(new Supplier
-			{
-				Name = "Autoplus",
-				City = "Ruse",
-				DiscountPercentage = 30.0m,
-				WebsiteUrl = "http://autoplus.bg"
-			});
-
-			context.Suppliers.Add(new Supplier
-			{
-				Name = "Kosser",
-				City = "Ruse",
-				DiscountPercentage = 35.0m,
-				WebsiteUrl = "http://kosser.bg"
-			});
-
-			context.Suppliers.Add(new Supplier
-			{
-				Name = "Autokelly",
-				City = "Varna",
-				DiscountPercentage = 20.0m,
-				WebsiteUrl = "http://autokelly.bg"
-			});
-
-			context.Suppliers.Add(new Supplier
-			{
-				Name = "Авто Комерс",
-				City = "Ruse",
-				DiscountPercentage = 28.5m,
-				WebsiteUrl = "http://autocommerce.bg"
-			});
+			#region Suppliers Seed Data
+			context.Suppliers.AddOrUpdate(s => s.SupplierId,
+				new Supplier
+				{
+					SupplierId = 1,
+					Name = "Euro07",
+					DiscountPercentage = 45.0m,
+					City = "Rousse",
+					WebsiteUrl = "http://euro07.bg"
+				},
+				new Supplier
+				{
+					SupplierId = 2,
+					Name = "AutoPlus",
+					DiscountPercentage = 33.0m,
+					City = "Rousse",
+					WebsiteUrl = "http://autoplus.bg"
+				},
+				new Supplier
+				{
+					SupplierId = 3,
+					Name = "AvtoKomers",
+					DiscountPercentage = 25.0m,
+					City = "Rousse",
+					WebsiteUrl = "http://acommers.bg"
+				},
+				new Supplier
+				{
+					SupplierId = 4,
+					Name = "MegaParts",
+					DiscountPercentage = 10.0m,
+					City = "Sofia",
+					WebsiteUrl = "http://megaparts.bg"
+				},
+				new Supplier
+				{
+					SupplierId = 5,
+					Name = "Inter Cars",
+					DiscountPercentage = 40.0m,
+					City = "Rousse",
+					WebsiteUrl = "http://intercars.bg"
+				}
+			);
+			#endregion
 
 			context.SaveChanges();
 
 			base.Seed(context);
-        }
-    }
+		}
+	}
 }
