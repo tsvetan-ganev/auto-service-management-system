1860f0216602ee6d7212d2acba03ef6ff36b8145
Tsvetan Ganev <cecoganev94@hotmail.com>
2015-02-07 18:32:26 +0200

    Custom validations improvement.

---
+++ AutoServiceManagementSystem/Validation/DisallowSpecialCharactersAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoServiceManagementSystem.Validation
+{
+	public class DisallowSpecialCharactersAttribute : ValidationAttribute
+	{
+		private readonly bool allowDigits;
+		private readonly char[] dissalowed = 
+			{ '!', '?', '#', '^', '*', '@', '~', '.', '%', '"', '\\', '/', ']', '[', '|', '+', '\n'};
+
+		public DisallowSpecialCharactersAttribute(bool allowDigits = true)
+			: base("{0} must not contain special symbols.")
+		{
+			this.allowDigits = allowDigits;
+			if (!allowDigits)
+			{
+				ErrorMessage = "{0} must not contain special symbols or numbers.";
+			}
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value != null)
+			{
+				string valueAsString = value.ToString();
+				foreach (char ch in valueAsString)
+				{
+					// if char isDigit() = true & allowDigits = false -> Error
+					// else ignore
+					if ((Char.IsSymbol(ch) || dissalowed.Contains(ch))
+						|| (Char.IsDigit(ch) && !allowDigits))
+					{
+						var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+						return new ValidationResult(errorMessage);
+					}
+				}
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
---
+++ AutoServiceManagementSystem/Validation/PhoneNumberAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoServiceManagementSystem.Validation
+{
+	public class PhoneNumberAttribute : ValidationAttribute
+	{
+		public PhoneNumberAttribute()
+			: base("Valid phone numbers follow this pattern: 08YXXXXXXX")
+		{
+
+		}
+
+		/// <summary>
+		/// Checks if a phone number is a valid one.
+		/// According to Bulgarian mobile operators.
+		/// 08YXXXXXXX - 10 digits
+		/// </summary>
+		/// <param name="value">Input phone number.</param>
+		/// <param name="validationContext">Describes the context in which the validation check is done.</param>
+		/// <returns>Success or ErrorMesage</returns>
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value != null)
+			{
+				string s = value.ToString();
+				bool valid = true;
+
+				if (s.StartsWith("08") && s.Length == 10)
+				{
+					foreach (char c in s)
+					{
+						if (!Char.IsDigit(c))
+						{
+							valid = false;
+							break;
+						}
+					}
+				}
+				else
+				{
+					valid = false;
+				}
+
+				if(!valid)
+				{
+					var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+					return new ValidationResult(errorMessage);
+				}
+
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
