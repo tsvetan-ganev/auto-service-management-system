8271649aa7ce5f0036df223992efb07005d92237
Tsvetan Ganev <cecoganev94@hotmail.com>
2015-02-07 18:19:11 +0200

    Removed unused properties.

--- AutoServiceManagementSystem/Models/Job.cs
+++ AutoServiceManagementSystem/Models/Job.cs
@@ -11,14 +11,10 @@ namespace AutoServiceManagementSystem.Models
     public class Job
     {
         private ICollection<SparePart> parts;
-        private OilChange oilChange;
-        private Labour labour;
 
         public Job()
         {
            this.parts = new List<SparePart>();
-           this.oilChange = new OilChange();
-           this.labour = new Labour();
         }
 
         public int JobId { get; set; }
@@ -39,9 +35,6 @@ namespace AutoServiceManagementSystem.Models
             get
             {
                 decimal result = Parts.Count > 0 ? Parts.Sum(p => p.Price) : 0;
-                result += OilChange.Price;
-                result += Labour.Price;
-
                 return result;
             }
         }
@@ -52,8 +45,6 @@ namespace AutoServiceManagementSystem.Models
             {
                 decimal totalProfit = Parts.Sum(p => p.Price) 
                     - Parts.Sum(p => p.YourPrice);
-                totalProfit += OilChange.Price;
-                totalProfit += Labour.Price;
                 return totalProfit;
             }
         }
@@ -62,19 +53,6 @@ namespace AutoServiceManagementSystem.Models
         {
             get { return parts; }
             set { parts = value; }
-        }
-
-        public virtual OilChange OilChange
-        {
-            get { return oilChange; }
-            set { oilChange = value; }
-        }
-
-		public int LabourId { get; set; }
-        public virtual Labour Labour
-        {
-            get { return labour; }
-            set { labour = value; }
         }
     }
 }
